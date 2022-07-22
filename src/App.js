874fe75ce3b4f6fa63fb9c2cb617b6abb81f7075
warrenhao <warren.vhao@gmail.com>
2022-07-22 18:22:24 +0800

    环境配置

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom'
+import Layout from './pages/Layout';
+import Login from './pages/Login';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-     
+      <Routes>
+        <Route path='/' element={<Layout/>}></Route>
+        <Route path='/login' element = {<Login/>}></Route>
+        
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
